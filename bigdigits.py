6f0a0952b8c6a64ca96133fde2ccd85a3c59f61f
Wenbo Liu <78084909+Waynelwb@users.noreply.github.com>
2021-03-08 20:41:06 +0800

    Add files via upload

--- bigdigits.py
+++ bigdigits.py
@@ -10,7 +10,7 @@
 # General Public License for more details.
 
 import sys
-
+# 从外部引入模块
 
 Zero = ["  ***  ",
         " *   * ",
@@ -34,16 +34,24 @@ Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
 try:
     digits = sys.argv[1]
+    # 从外部获得的参数组成的一个列表（list)，也就是说sys.argv其实可以看作是一个列表，所以才能用[]提取其中的元素。
+    # 其第一个元素是程序本身，随后才依次是外部给予的参数。
     row = 0
     while row < 7:
         line = ""
+        # 空字符串
         column = 0
         while column < len(digits):
             number = int(digits[column])
+            # 检索输入的数字
             digit = Digits[number]
+            # 得到输入数字对应的列表
             line += digit[row] + "  "
+            # 得到各个数字的第一行打印
             column += 1
+            # 开启循环，得到各个数字的完整打印
         print(line)
+        # 打印出完整的数字表达
         row += 1
 except IndexError:
     print("usage: bigdigits.py <number>")
